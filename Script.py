8720a91895e7d2f2117c34ff7ad3e30bdab8c980
CinemaLover2 <devayt0811@gmail.com>
2026-07-06 12:42:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -5,7 +5,7 @@
 class script(object):
     START_TXT = """<b><blockquote>ʜᴇʟʟᴏ {} 👋,</blockquote>
     
-ɪ ᴀᴍ ʟᴀᴛᴇꜱᴛ ᴀᴅᴠᴀɴᴄᴇᴅ ᴀɴᴅ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏ ꜰɪʟᴛᴇʀ ʙᴏᴛ.Just tell me movies or web series name correctly...</b>"""
+WELCOME to Filmy Quest ʙᴏᴛ (@DHPfilmyBot).Just tell me movies or web series name in this group @dhpmovierequest correctly...</b>"""
 
     CLONE_START_TXT = """<b><blockquote>ʜᴇʟʟᴏ {}, ᴍʏ ɴᴀᴍᴇ <a href=https://t.me/{}>{}</a></blockquote>
     
